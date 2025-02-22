105eb6f94b76637659399f434379e3f77dce6d58
Dharsan <dharsan.s0503@gmail.com>
2025-02-22 23:39:28 +0530

    dbconnection

--- Backend/server.js
+++ Backend/server.js
@@ -2,13 +2,14 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const authRoutes = require("./routes/auth");
-
+const connectDB = require("./config/db");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
+connectDB();
 app.use("/api/auth",authRoutes);
 
 const PORT = process.env.PORT || 3000;
